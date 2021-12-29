9a322a935fa698c0d1b081fc1f6826a6a0bfa6b7
sshsphere <47260097+sshsphere@users.noreply.github.com>
2021-12-30 01:07:04 +0200

    Update

--- script.js
+++ script.js
@@ -22,7 +22,16 @@ const Gameboard = (()=>{
         possibleMoves=new Set([0,1,2,3,4,5,6,7,8]);
     }
     const checkWin=()=>{
-
+        if      (board[0][0] == board[0][1] && board[0][1] == board[0][2]) return board[0][0]; //first row
+        else if (board[1][0] == board[1][1] && board[1][1] == board[1][2]) return board[1][0]; //second row
+        else if (board[2][0] == board[2][1] && board[2][1] == board[1][2]) return board[2][0]; //third row
+        else if (board[0][0] == board[1][0] && board[1][0] == board[2][0]) return board[0][0]; //first column
+        else if (board[0][1] == board[1][1] && board[1][1] == board[2][1]) return board[0][1]; //second column
+        else if (board[0][2] == board[1][2] && board[1][2] == board[2][2]) return board[0][2]; //third column
+        else if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) return board[0][0]; //first diagonal
+        else if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) return board[0][2]; //second diagonal
+        else if (!board.some(row => row.includes(""))) return "Tie"; 
+        else return {"Ongoing", -1}; 
     }
     return{getBoard, setBoardPos, clearBoard, isValid};
 })();
@@ -40,25 +49,25 @@ const GameState = (()=>{
     let turn=false;
     const playerX=Player(playername, "X");
     const playerO=Player(playername, "O");
-    const squares=document.querySelectorAll(".grid-item");
+    const boards=document.querySelectorAll(".grid-item");
     const Move=(ind)=>{
         if(!Gameboard.isValid(ind)) return;
-        const currentSquare=document.querySelector(`.grid-item[data-ind="${ind}"]`);
+        const currentboard=document.querySelector(`.grid-item[data-ind="${ind}"]`);
         if(turn==0){
             playerX.makeMove(ind);
-            currentSquare.textContent="X";
+            currentboard.textContent="X";
             turn=!turn;
             
         }
         else{
             playerO.makeMove(ind);
-            currentSquare.textContent="O";
+            currentboard.textContent="O";
             turn=!turn;
         }
     }
-    squares.forEach((square)=>{
-        square.addEventListener('click', ()=>{
-            Move(square.dataset.ind);
+    boards.forEach((board)=>{
+        board.addEventListener('click', ()=>{
+            Move(board.dataset.ind);
             
         });
     });
